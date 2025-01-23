c6c610a8553474f609cd8b5a0d9290dcb43a0df1
Smaksh-09 <smakshdhawan@gmail.com>
2025-01-23 18:39:55 +0530

    fixed some api issues

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,7 +1,7 @@
 export const getTranslationApiUrl = () => {
   if (process.env.NODE_ENV === 'production') {
    
-    return 'http://localhost:5000/?source=auto&target=el&q=yo';
+    return 'http://localhost:5000';
   }
-  return 'http://localhost:5000/translate';
+  return 'http://localhost:5000';
 };
